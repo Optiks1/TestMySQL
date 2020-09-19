f76c69c7cc582076071a133582bedf4ce003160e
Optiks1 <71526015+Optiks1@users.noreply.github.com>
2020-09-19 18:35:33 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -72,7 +72,7 @@ function showWindow()
 		end
 	--end
 end
-bindKey("F2","down",showWindow)
+bindKey("L","down",showWindow)
 
 function dgsGridListGetSelectedItemText(gridList,column)
     local item = dgsGridListGetSelectedItem(gridList)
